66f422f0790d70140e2ec29a97d552b55e944cdd
Mikko Kokkonen <mikko@mikian.com>
2012-04-12 16:12:39 +0300

    Retina display support

--- lib/alula/engine/plugins/lightbox.rb
+++ lib/alula/engine/plugins/lightbox.rb
@@ -3,7 +3,7 @@ require 'mini_exiftool'
 module Alula
   class Engine
     class Plugins
-      class Lightbox < Tag
+      class Lightbox < Image
         register :lightbox
         
         def self.install(options)
@@ -13,27 +13,37 @@ module Alula
           File.expand_path(File.join(File.dirname(__FILE__), *%w{.. .. .. .. plugins lightbox}))
         end
         
-        def prepare(markup, tokens)
-          /(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?<title>\s+.+)?/ =~ markup
-          /(?:"|')(?<title>[^"']+)?(?:"|')\s+(?:"|')(?<alt>[^"']+)?(?:"|')/ =~ title
-          
-          @name = src
-          @title = title || ""
-          @alt = alt || ""
-        end
+        # def prepare(markup, tokens)
+        #   /(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?<title>\s+.+)?/ =~ markup
+        #   /(?:"|')(?<title>[^"']+)?(?:"|')\s+(?:"|')(?<alt>[^"']+)?(?:"|')/ =~ title
+        #   
+        #   @name = src
+        #   @title = title || ""
+        #   @alt = alt || ""
+        # end
         
         def content(context)
-          image = context.asset_url(File.join("images", @name))
-          thumbnail = context.asset_url(File.join("thumbnails", @name))
+          # image = context.asset_url(File.join("images", @name))
+          # thumbnail = context.asset_url(File.join("thumbnails", @name))
+          image = source(@name, "images")
+          thumbnail = source(@name, "thumbnails")
           
           unless image and thumbnail
             "<!-- Image #{@name} cannot be found. -->"
           else
-            exif = MiniExiftool.new File.join(context.config.public_path, thumbnail)
+            # binding.pry
+            exif = MiniExiftool.new File.join(context.config.public_path, image)
             width, height = exif.imagewidth, exif.imageheight
             
-            tag = "<a class=\"fancybox\" rel=\"#{context.page.id}\" href=\"#{image}\">"
-            tag += "<img src=\"#{thumbnail}\" alt=\"#{@alt}\" title=\"#{@title}\" width=\"#{width}\" height=\"#{height}\">"
+            tag = "<a"
+            tag += " class=\"fancybox\""
+            tag += " rel=\"#{context.page.id}\""
+            tag += " href=\"#{image}\""
+            tag += " data-width=\"#{width}\""
+            tag += " data-height=\"#{height}\""
+            tag += " data-retina=\"#{retina(@name, "images")}\"" if context.config.images["retina"] and retina(@name, "images")
+            tag += ">"
+            tag += imagetag(@name, 'thumbnails')
             tag += "</a>"
           end
         end
--- plugins/lightbox/assets/javascripts/lightbox.js.coffee
+++ plugins/lightbox/assets/javascripts/lightbox.js.coffee
@@ -1,5 +1,15 @@
-//=require jquery
-//=require jquery.fancybox
+#=require jquery
+#=require jquery.fancybox
 
 jQuery ->
-	$(".fancybox").fancybox()
+	if $.getDevicePixelRatio() > 1
+		$(".fancybox").each ->
+			if $(this).data('retina')
+				$(this).attr 'href', $(this).data 'retina'
+	
+	$(".fancybox").fancybox
+		beforeLoad: ->
+			if @element.attributes['data-width']
+				@maxWidth = @element.attributes['data-width'].value
+			if @element.attributes['data-height']
+				@maxHeight = @element.attributes['data-height'].value
