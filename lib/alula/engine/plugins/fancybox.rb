43e9cc4913fbc152b3b052c746cf36c76b890bd3
Mikko Kokkonen <mikko@mikian.com>
2012-04-13 22:20:42 +0300

    Renamed lightbox plugin to fancybox (as that it is)

---
+++ lib/alula/engine/plugins/fancybox.rb
@@ -0,0 +1,63 @@
+require 'mini_exiftool'
+
+module Alula
+  class Engine
+    class Plugins
+      class Fancybox < Image
+        register :image
+        
+        def self.install(options)
+          @@options = options
+          
+          # Display notice
+          unless options.kind_of?(Hash) and options["personal"]
+            puts <<-end_of_notice
+*** fancyBox
+Please note, that fancyBox is licensed under the therms of the
+Creative Commons Attribution-NonCommercial 3.0 License
+(http://creativecommons.org/licenses/by-nc/3.0/).
+
+If you would like to use fancyBox for commercial purposes,
+you can purchase a license from http://fancyapps.com/store/
+              
+To remove this notice, please include following options in config.yml
+---
+plugins:
+  fancybox:
+    personal: true
+  
+              end_of_notice
+          end
+          
+          # Return path to assets
+          File.expand_path(File.join(File.dirname(__FILE__), *%w{.. .. .. .. plugins fancybox}))
+        end
+        
+        def content(context)
+          image = source(@src, "images")
+          thumbnail = source(@src, "thumbnails")
+          
+          unless image and thumbnail
+            # Not an attachment, fall back to regular image tag
+            super
+          else
+            # binding.pry
+            exif = MiniExiftool.new File.join(context.config.public_path, image)
+            width, height = exif.imagewidth, exif.imageheight
+            
+            tag = "<a"
+            tag += " class=\"fancybox\""
+            tag += " rel=\"#{context.page.id}\""
+            tag += " href=\"#{image}\""
+            tag += " data-width=\"#{width}\""
+            tag += " data-height=\"#{height}\""
+            tag += " data-retina=\"#{hires(@src, "images")}\"" if context.config.images["hires"] and hires(@src, "images")
+            tag += ">"
+            tag += imagetag(@src, 'thumbnails')
+            tag += "</a>"
+          end
+        end
+      end
+    end
+  end
+end
