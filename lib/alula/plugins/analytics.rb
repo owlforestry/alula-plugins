a45d4bf44145e8923e7d386fb488f148cc5e158b
Mikko Kokkonen <mikko@mikian.com>
2012-07-19 23:08:22 +0300

    Added support for woopra analytics

--- lib/alula/plugins/analytics.rb
+++ lib/alula/plugins/analytics.rb
@@ -10,23 +10,17 @@ module Alula
       return false unless options.kind_of?(Hash)
       
       options.each do |provider, opts|
-        if provider == "gosquared"
-          Alula::Plugin.addon(:body, ->(context) {<<-EOT
-            <script type="text/javascript">
-              var GoSquared={};
-              GoSquared.acct = "#{opts}";
-              (function(w){
-                function gs(){
-                  w._gstc_lt=+(new Date); var d=document;
-                  var g = d.createElement("script"); g.type = "text/javascript"; g.async = true; g.src = "//d1l6p2sc9645hc.cloudfront.net/tracker.js";
-                  var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(g, s);
-                }
-                w.addEventListener?w.addEventListener("load",gs,false):w.attachEvent("onload",gs);
-              })(window);
-            </script>
+        tracker = case provider
+        when "gosquared"
+          <<-EOT
+            <script type="text/javascript">var GoSquared={};GoSquared.acct="#{opts}",function(e){function t(){e._gstc_lt=+(new Date);var t=document,n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="//d1l6p2sc9645hc.cloudfront.net/tracker.js";var r=t.getElementsByTagName("script")[0];r.parentNode.insertBefore(n,r)}e.addEventListener?e.addEventListener("load",t,!1):e.attachEvent("onload",t)}(window);</script>
             EOT
-            })
+        when "woopra"
+          <<-EOT
+          <script type="text/javascript">function woopraReady(e){return e.setDomain("#{opts}"),e.setIdleTimeout(3e5),e.track(),!1}(function(){var e=document.createElement("script");e.src=document.location.protocol+"//static.woopra.com/js/woopra.js",e.type="text/javascript",e.async=!0;var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();</script>
+          EOT
         end
+        Alula::Plugin.addon(:body, ->(context) { tracker }) if tracker
       end
     end
   end
