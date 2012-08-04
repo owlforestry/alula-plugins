77ca2bc18394a14b1222dc0c979c71e0fe7826d5
Mikko Kokkonen <mikko@mikian.com>
2012-08-04 18:34:50 +0300

    Added support for chartbeat

--- lib/alula/plugins/analytics.rb
+++ lib/alula/plugins/analytics.rb
@@ -15,6 +15,11 @@ module Alula
       
       options.each do |provider, opts|
         tracker = case provider
+        when "chartbeat"
+          Alula::Plugin.addon :head, ->(context) { "<script type=\"text/javascript\">var _sf_startpt=(new Date()).getTime()</script>"}
+          <<-EOT
+          <script type="text/javascript">var _sf_async_config={uid:#{opts['uid']},domain:"#{opts['domain']}"};(function(){function e(){window._sf_endpt=(new Date).getTime();var e=document.createElement("script");e.setAttribute("language","javascript"),e.setAttribute("type","text/javascript"),e.setAttribute("src",("https:"==document.location.protocol?"https://a248.e.akamai.net/chartbeat.download.akamai.com/102508/":"http://static.chartbeat.com/")+"js/chartbeat.js"),document.body.appendChild(e)}var t=window.onload;window.onload=typeof window.onload!="function"?e:function(){t(),e()}})();</script>
+          EOT
         when "gosquared"
           <<-EOT
             <script type="text/javascript">var GoSquared={};GoSquared.acct="#{opts}",function(e){function t(){e._gstc_lt=+(new Date);var t=document,n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="//d1l6p2sc9645hc.cloudfront.net/tracker.js";var r=t.getElementsByTagName("script")[0];r.parentNode.insertBefore(n,r)}e.addEventListener?e.addEventListener("load",t,!1):e.attachEvent("onload",t)}(window);</script>
